e9fae34bffe38d702775d14349e9951386e736df
dmbuagsu <atbakcii7@gmail.com>
2026-04-19 21:04:07 +0300

    Basic UI with buttons and text fields

--- MazeCore/MainWindow.xaml.cs
+++ MazeCore/MainWindow.xaml.cs
@@ -1,24 +1,17 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace MazeCore
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Ви ввели: {TestInput.Text}", "Тест UI");
         }
     }
 }
